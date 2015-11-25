8899ae77e193ce1a580fdba50a62bcc9e8084280
Ben MacKenzie <ben.mackenzie@thinkbiganalytics.com>
2015-11-25 16:19:37 -0500

    get schema from jar

--- src/data_profile/schema.clj
+++ src/data_profile/schema.clj
@@ -1,9 +1,11 @@
-(ns data-profile.schema)
+(ns data-profile.schema
+  (:require [clojure.java.io :as io]))
 
 
 
  ;reading schema in from file has caused me to loose the max int contant...how to do this properly
+ ;assume schema is in resources directory
  (defn get-schema [name]
-  (read-string (slurp name)))
+  (read-string (io/file (io/resource name))))
 
 
