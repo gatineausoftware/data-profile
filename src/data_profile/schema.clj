906bdade2df1d7749b083a04c702a1ad87e724d5
Ben MacKenzie <ben.mackenzie@thinkbiganalytics.com>
2015-11-25 16:40:56 -0500

    changed get-schema

--- src/data_profile/schema.clj
+++ src/data_profile/schema.clj
@@ -6,6 +6,6 @@
  ;reading schema in from file has caused me to loose the max int contant...how to do this properly
  ;assume schema is in resources directory
  (defn get-schema [name]
-  (read-string (io/file (io/resource name))))
+  (read-string (slurp (io/resource name))))
 
 
