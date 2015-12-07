16b41296852f02a56630ad628e5578b736309025
Ben MacKenzie <ben.mackenzie@thinkbiganalytics.com>
2015-12-07 09:24:55 -0500

    re-structured profile map

--- src/data_profile/profile.clj
+++ src/data_profile/profile.clj
@@ -8,53 +8,62 @@
   (:use       [data-profile.util]))
 
 
-;;this version of profile just uses spark to pull back a sample of data.  profiling is then executed in driver
+
+ (defn pmin [a b]
+   (cond
+    (= a :none) b
+    (= b :none) a
+    :else (min a b)))
 
 
- ;need better name
- ;add max and min for decimal
- ;fix min and max for date, integer
 
- (def a {:missing 0 :date {:count 0 :min -1 :max 1} :integer {:count 0 :min 0 :max 0} :string {:max_length 0} :decimal {:count 0 :max_scale 0}})
+ (defn pmax [a b]
+   (cond
+    (= a :none) b
+    (= b :none) a
+    :else (max a b)))
 
 
- (def c {:count 0 :missing 0 :max_length 0
-         :numeric {:num_integer 0 :num_decimal 0 :max 0 :min 0 :max_scale 0}
+ (def a {:count 0 :missing 0 :max_length 0
+         :numeric {:count 0 :integer_count 0  :min :none :max :none :min_scale 0 :max_scale 0}
          :date {:count 0 :earliest 0 :latest 0}})
 
 
  ;;count column is counting size of column...if it is zero, it's missing
  (defn profile-missing [column c-profile]
    (if (zero? (count column))
-              (update-in c-profile [:missing] + 1) c-profile))
+              (update-in c-profile [:missing] + 1) (update-in c-profile [:count] + 1)))
+
+ (defn profile-string [column c-profile]
+   (update-in c-profile [:max_length] max (count column)))
+
+
+ (defn profile-numeric [column c-profile]
+   (if-let [d (getDecimal column)]
+     (->
+      c-profile
+      (update-in [:numeric :count] + 1)
+      (update-in [:numeric :max_scale] max (.scale d))
+      (update-in [:numeric :min_scale] min (.scale d))
+      (update-in [:numeric :min] pmin d)
+      (update-in [:numeric :max] pmax d)
+      ) c-profile))
 
 
  (defn profile-integer [column c-profile]
   (if-let [int-col (getInteger column)]
    (->
       c-profile
-     (update-in [:integer :count] + 1)
-     (update-in [:integer :max] max int-col)
-     (update-in [:integer :min] min int-col)
+     (update-in [:numeric :integer_count] + 1)
     )
     c-profile))
 
-
-
- (defn profile-string [column c-profile]
-   (update-in c-profile [:string :max_length] max (count column)))
 
  (defn profile-date [column c-profile]
    (if (isDate? column)
      (update-in c-profile [:date :count] + 1) c-profile))
 
- (defn profile-decimal [column c-profile]
-   (if-let [d (getDecimal column)]
-     (->
-      c-profile
-      (update-in [:decimal :count] + 1)
-      (update-in [:decimal :max_scale] max (.scale d))
-      ) c-profile))
+
 
  (defn profile-column [c-profile column]
    (->>
@@ -63,23 +72,26 @@
     (profile-integer column)
     (profile-string column)
     (profile-date column)
-    (profile-decimal column)
+    (profile-numeric column)
     ))
 
 
- (defn l-profile-row [profile row]
-   (if (not= (count profile) (count row)) profile
-     (loop [cp profile c row res []]
+ (defn profile-row [num_columns profile row]
+   (if (not= (count row) num_columns) profile
+      (loop [cp profile c row res []]
        (if cp (recur (next cp) (next c) (conj res (profile-column (first cp) (first c)))) res))))
+
+
 
 
   ;;should probably get a distribution of row size and take the most common...note that hcat client just takes the max
  (defn get-num-columns [rows]
    (count (first rows)))
 
-  ;;initialize max and min with first row?
   (defn profile-data [rows]
-    (reduce l-profile-row (repeat (get-num-columns rows) a) rows))
+    (let [num_columns (get-num-columns rows)]
+    (reduce (partial profile-row num_columns) (repeat num_columns a) rows)))
+
 
 
   (defn profile-with-options [rdd {:keys [delimiter sample]}]
@@ -107,6 +119,10 @@
 
 
 
- ;;this also can blow up if sample is large enough
- ;;(defn profile-row [profile row]
- ;;(map profile-column profile row))
+;;will dorun fix stackoverflow problem?  maybe...but do run is only good
+ ;;for side effects....returns nil
+ ;(defn profile-row [num_columns profile row]
+  ; (if (= (count row) num_columns)
+     ;(dorun (map profile-column profile row)) profile))
+
+
--- test/data_profile/profile_test.clj
+++ test/data_profile/profile_test.clj
@@ -23,8 +23,7 @@
     (is (=
          (->
           p/a
-          (assoc-in [:integer :count] 1)
-          (assoc-in [:integer :max] 1001))
+          (assoc-in [:numeric :integer_count] 1))
           (p/profile-integer "1001" p/a))))
 
 
@@ -37,7 +36,7 @@
     (is (=
          (->
           p/a
-          (assoc-in [:string :max_length] 4))
+          (assoc-in [:max_length] 4))
           (p/profile-string "1001" p/a))))
 
 
@@ -45,29 +44,32 @@
     (is (=
          (->
           p/a
-          (assoc-in [:decimal :count] 1)
-          (assoc-in [:decimal :max_scale] 2))
-          (p/profile-decimal "2.13" p/a))))
+          (assoc-in [:numeric :count] 1)
+          (assoc-in [:numeric :min] 2.13M)
+          (assoc-in [:numeric :max] 2.13M)
+          (assoc-in [:numeric :max_scale] 2))
+          (p/profile-numeric "2.13" p/a))))
 
   (testing
     (is (=
-         p/a
-        (p/profile-decimal "not a number" p/a))))
+        (p/profile-numeric "not a number" p/a))))
 
   (testing
     (is (=
          (->
           p/a
-          (assoc-in [:integer :count] 1)
-          (assoc-in [:integer :max] 42)
-          (assoc-in [:string :max_length] 2)
-          (assoc-in [:decimal :count] 1))
+          (assoc-in [:count] 1)
+          (assoc-in [:max_length] 2)
+          (assoc-in [:numeric :count] 1)
+          (assoc-in [:numeric :max] 42M)
+          (assoc-in [:numeric :min] 42M)
+          (assoc-in [:numeric :integer_count] 1))
           (p/profile-column p/a "42"))))
 
   )
 
 
-;;problems: "15,000" is not parsing as an int
+
 
 (def testdata2
   [["1001" "atlanta" "2015-10-31" "2.01" "15000"]
@@ -76,35 +78,45 @@
 (def testresult2
    (vector (->
          p/a
-         (assoc-in [:string :max_length] 4)
-         (assoc-in [:integer :count] 2)
-         (assoc-in [:integer :max] 1002)
-         (assoc-in [:decimal :count] 2)
-         (assoc-in [:decimal :max_scale] 0))
+         (assoc-in [:count] 2)
+         (assoc-in [:max_length] 4)
+         (assoc-in [:numeric :integer_count] 2)
+         (assoc-in [:numeric :max] 1002M)
+         (assoc-in [:numeric :min] 1001M)
+         (assoc-in [:numeric :count] 2))
+
 
         (->
          p/a
-         (assoc-in [:string :max_length] 7))
+         (assoc-in [:count] 2)
+         (assoc-in [:max_length] 7))
 
         (->
          p/a
-         (assoc-in [:string :max_length] 10)
+         (assoc-in [:count] 2)
+         (assoc-in [:max_length] 10)
          (assoc-in [:date :count] 2))
 
         (->
          p/a
-         (assoc-in [:string :max_length] 4)
-         (assoc-in [:decimal :count] 2)
-         (assoc-in [:decimal :max_scale] 2))
+         (assoc-in [:count] 2)
+         (assoc-in [:max_length] 4)
+         (assoc-in [:numeric :count] 2)
+         (assoc-in [:numeric :max_scale] 2)
+         (assoc-in [:numeric :max] 3.2M)
+         (assoc-in [:numeric :min] 2.01M))
 
          (->
           p/a
-         (assoc-in [:string :max_length] 5)
-         (assoc-in [:integer :count] 1)
-         (assoc-in [:integer :max] 15000)
-         (assoc-in [:decimal :count] 1)
+         (assoc-in [:count] 1)
          (assoc-in [:missing] 1)
-        )))
+         (assoc-in [:max_length] 5)
+         (assoc-in [:numeric :integer_count] 1)
+         (assoc-in [:numeric :max] 15000M)
+         (assoc-in [:numeric :min] 15000M)
+         (assoc-in [:numeric :count] 1))
+
+        ))
 
 
 
@@ -148,7 +160,7 @@
       (is (= 23
         (->
         (s/text-file sc "resources/sample.csv")
-        (p/profile)
+        (p/profile [1 ","])
         count
         ))))))
 
